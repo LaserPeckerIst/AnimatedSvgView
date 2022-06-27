ccf2d83f3b23a15cc38f377ab8550d38a607c65c
angcyo <angcyo@126.com>
2022-06-27 13:58:51 +0800

    * format

--- demo/src/main/java/com/jaredrummler/android/animatedsvgview/demo/MainActivity.java
+++ demo/src/main/java/com/jaredrummler/android/animatedsvgview/demo/MainActivity.java
@@ -18,70 +18,76 @@
 package com.jaredrummler.android.animatedsvgview.demo;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.jaredrummler.android.widget.AnimatedSvgView;
 
 public class MainActivity extends AppCompatActivity {
 
-  /*package*/ AnimatedSvgView svgView;
-  /*package*/ int index = -1;
+    /*package*/ AnimatedSvgView svgView;
+    /*package*/ int index = -1;
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    svgView = (AnimatedSvgView) findViewById(R.id.animated_svg_view);
+        svgView = (AnimatedSvgView) findViewById(R.id.animated_svg_view);
 
-    svgView.postDelayed(new Runnable() {
+        svgView.postDelayed(new Runnable() {
 
-      @Override public void run() {
+            @Override
+            public void run() {
+                svgView.start();
+            }
+        }, 500);
+
+        svgView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (svgView.getState() == AnimatedSvgView.STATE_FINISHED) {
+                    svgView.start();
+                }
+            }
+        });
+
+        svgView.setOnStateChangeListener(new AnimatedSvgView.OnStateChangeListener() {
+
+            @Override
+            public void onStateChange(@AnimatedSvgView.State int state) {
+                if (state == AnimatedSvgView.STATE_TRACE_STARTED) {
+                    findViewById(R.id.btn_previous).setEnabled(false);
+                    findViewById(R.id.btn_next).setEnabled(false);
+                } else if (state == AnimatedSvgView.STATE_FINISHED) {
+                    findViewById(R.id.btn_previous).setEnabled(index != -1);
+                    findViewById(R.id.btn_next).setEnabled(true);
+                    if (index == -1) index = 0; // first time
+                }
+            }
+        });
+    }
+
+    public void onNext(View view) {
+        if (++index >= SVG.values().length) index = 0;
+        setSvg(SVG.values()[index]);
+    }
+
+    public void onPrevious(View view) {
+        if (--index < 0) index = SVG.values().length - 1;
+        setSvg(SVG.values()[index]);
+    }
+
+    private void setSvg(SVG svg) {
+        svgView.setGlyphStrings(svg.glyphs);
+        svgView.setFillColors(svg.colors);
+        svgView.setViewportSize(svg.width, svg.height);
+        svgView.setTraceResidueColor(0x32000000);
+        svgView.setTraceColors(svg.colors);
+        svgView.rebuildGlyphData();
         svgView.start();
-      }
-    }, 500);
-
-    svgView.setOnClickListener(new View.OnClickListener() {
-
-      @Override public void onClick(View v) {
-        if (svgView.getState() == AnimatedSvgView.STATE_FINISHED) {
-          svgView.start();
-        }
-      }
-    });
-
-    svgView.setOnStateChangeListener(new AnimatedSvgView.OnStateChangeListener() {
-
-      @Override public void onStateChange(@AnimatedSvgView.State int state) {
-        if (state == AnimatedSvgView.STATE_TRACE_STARTED) {
-          findViewById(R.id.btn_previous).setEnabled(false);
-          findViewById(R.id.btn_next).setEnabled(false);
-        } else if (state == AnimatedSvgView.STATE_FINISHED) {
-          findViewById(R.id.btn_previous).setEnabled(index != -1);
-          findViewById(R.id.btn_next).setEnabled(true);
-          if (index == -1) index = 0; // first time
-        }
-      }
-    });
-  }
-
-  public void onNext(View view) {
-    if (++index >= SVG.values().length) index = 0;
-    setSvg(SVG.values()[index]);
-  }
-
-  public void onPrevious(View view) {
-    if (--index < 0) index = SVG.values().length - 1;
-    setSvg(SVG.values()[index]);
-  }
-
-  private void setSvg(SVG svg) {
-    svgView.setGlyphStrings(svg.glyphs);
-    svgView.setFillColors(svg.colors);
-    svgView.setViewportSize(svg.width, svg.height);
-    svgView.setTraceResidueColor(0x32000000);
-    svgView.setTraceColors(svg.colors);
-    svgView.rebuildGlyphData();
-    svgView.start();
-  }
+    }
 
 }
